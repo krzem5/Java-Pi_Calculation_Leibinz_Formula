4f25a1942cfaf460dd96dc9c58070f02665acd41
Krzem <krzem5.dev@gmail.com>
2021-04-10 10:12:33 +0200

    Push Update 04/10/2021, 10:12:14

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/pi_calculation_leibinz_formula/Main.java&&jar cvmf ../manifest.mf ../build/pi_calculation_leibinz_formula.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="pi_calculation_leibinz_formula.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/pi_calculation_leibinz_formula.jar
-:end
+python build.py %*
